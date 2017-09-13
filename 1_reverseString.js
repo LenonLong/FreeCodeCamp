1629166be2912a9fd313c860fb86089e9fe7d4a8
Lenon Long <amronglong@gmail.com>
2017-09-13 16:38:52 -0700

    Delete 1_reverseString.js

--- 1_reverseString.js
+++
@@ -1,8 +0,0 @@
-// Reverse the provided string
-function reverseStr(str){
-// .split will split the individual letter / .join will join them back together
-  var newStr = str.split('').reverse().join('');
-  return newStr;
-}
-
-reverseStr('hello');
